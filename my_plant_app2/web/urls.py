b766219e1f3bff62091718a4c3cba0c376e4ea54
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-16 11:57:21 +0200

    Added home page

--- my_plant_app2/web/urls.py
+++ my_plant_app2/web/urls.py
@@ -2,17 +2,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from my_plant_app2.web.views import index, profile_create, profile_details, profile_edit, profile_delete, catalogue
+from my_plant_app2.web.views import index, profile_create, profile_details, profile_edit, profile_delete, catalogue, \
+    create_plant, details_plant, edit_plant, delete_plant
 
 urlpatterns = [
- path("", index, name="home page"),
- path('profile/', include([
-  path('create/', profile_create, name="create profile"),
-  path('details/', profile_details, name="details profile"),
-  path('edit/', profile_edit , name="edit profile"),
-  path('delete/', profile_delete, name="delete profile"),
- ])),
- path("catalogue/", catalogue, name="catalogue"),
+    path("", index, name="home page"),
+    path('profile/', include([
+        path('create/', profile_create, name="create profile"),
+        path('details/', profile_details, name="details profile"),
+        path('edit/', profile_edit , name="edit profile"),
+        path('delete/', profile_delete, name="delete profile"),
+        ])),
+    path('catalogue/', catalogue, name="catalogue"),
+    path('create/', create_plant, name="create plant"),
+    path('details/<int:pk>/', details_plant, name="details plant"),
+    path('edit/<int:pk>/', edit_plant, name="edit plant"),
+    path('delete/<int:pk>/', delete_plant, name="delete plant"),
 
 ]
-
--- my_plant_app2/web/views.py
+++ my_plant_app2/web/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    pass
+    return render(request, "home-page.html")
 
 
 def profile_create(request):
@@ -21,4 +21,17 @@ def profile_delete(request):
 
 
 def catalogue(request):
+    pass
+
+def create_plant(request):
+    pass
+
+def edit_plant(request, pk):
+    pass
+
+def delete_plant(request, pk):
+    pass
+
+
+def details_plant(request, pk):
     pass
